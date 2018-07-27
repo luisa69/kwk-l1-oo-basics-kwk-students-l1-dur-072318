ef009f23a2d703db1d2d0f0452245bb46ee7c05f
Luisa Pichardo <luisapichardo@gmail.com>
2018-07-27 13:18:50 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
 # book.rb
 # Add you Book class here
+
+class Book 
+  def initialize(title, author, page_count)
+    @title 
+    @author
+    @page_count
+  end 
+  
+  def title 
+    @title
+  end 
+  
+  def author
+    @author
+  end 
+  
+  def page_count
+    @page_count
+  end 
+  
+  
+  
+  
+    
+  
